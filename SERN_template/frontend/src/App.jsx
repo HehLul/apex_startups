18182b7a1bb8271379c7c6efbeb6869755b7b26d
Ahmed Salman <amdsalman04@gmail.com>
2025-09-29 20:23:42 -0400

    refactor(sern_fe): simplify boilerplate

---
+++ SERN_template/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage/LandingPage"
+
+function App() {
+ 
+
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App
---
+++ SERN_template/frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,9 @@
+export default function LandingPage(){
+    return(
+        <>
+        <div className="landing-page">
+            <h1>Where SaaS ideas are devloped within seconds!</h1>
+        </div>
+        </>
+    );
+}
